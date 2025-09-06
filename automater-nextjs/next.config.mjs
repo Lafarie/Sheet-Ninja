181ccd8058f115cb0fd257ecf9ae5e0c10632996
Farhad Lafarie <farhadlafarie@gmail.com>
2025-09-06 19:22:48 +0530

    feat(docker): refactor Dockerfile and docker-compose for improved build process; update .dockerignore and add sdoc.txt for documentation

--- automater-nextjs/next.config.mjs
+++ automater-nextjs/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
